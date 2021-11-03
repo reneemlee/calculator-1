abe4b4b262a08a1f417fc7fff9237de70cfb133f
Renee <reneemlee92@gmail.com>
2021-11-03 19:13:40 +0000

    fixed power func

--- calculator-1/arithmetic.py
+++ calculator-1/arithmetic.py
@@ -31,9 +31,11 @@ def cube(num1):
     """Return the cube of the input."""
 
     return num1 ** 3
+
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
-
+    return num1 ** num2
+    
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
